f903bd26068fe459125e3609b6c4645f32152083
PatriciaRuffino <sindelformr@gmail.com>
2022-08-26 12:13:19 -0300

    projeto gamaAcademy js finalizado

---
+++ database.js
@@ -0,0 +1,40 @@
+const livros = [
+    {
+    id:1,
+    nome: "Digital Minimalism",
+    autor: "Cal Newport",
+    categoria: "Produtividade e estilo de vida",
+    paginas: 254,
+    recomenta: false,
+    leu: false
+},
+{
+    id:2,
+    nome: "Sandman",
+    autor: "Neilman",
+    categoria: "Ficção",
+    paginas: 300,
+    recomenta: false,
+    leu: false
+},
+{
+    id:3,
+    nome: "Lógica de Programação",
+    autor: "Jhon Smith",
+    categoria: "TI",
+    paginas: 500,
+    recomenta: false,
+    leu: false
+},
+{
+    id:4,
+    nome: "Digital Minimalism",
+    autor: "Cal Newport",
+    categoria: "Produtividade e estilo de vida",
+    paginas: 254,
+    recomenta: false,
+    leu: false
+},
+]
+
+module.exports = livros
